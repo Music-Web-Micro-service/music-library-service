e86c6f9ef5ac5a6898822ffc3fdaf9a4aa988d5f
ZHallen122 <zhallen12261@gmail.com>
2023-09-16 22:10:53 -0400

    fix bug

--- src/main/java/com/freemusic/musiclibraryservice/services/implement/AlbumServiceImpl.java
+++ src/main/java/com/freemusic/musiclibraryservice/services/implement/AlbumServiceImpl.java
@@ -21,7 +21,7 @@ public class AlbumServiceImpl implements AlbumService {
 
     @Override
     public Album getAlbumById(int albumId) {
-        return albumRepository.getReferenceById(albumId);
+        return albumRepository.findById(albumId).orElse(null);
     }
 
     @Override
